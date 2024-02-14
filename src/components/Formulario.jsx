e52cd3de74939cbbb6de61918abe4b1ee219ce2c
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-02-14 19:00:15 -0300

    consulta API externa

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import { monedas } from "../data/monedas";
@@ -13,6 +14,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover {
         background-color: #7A7DFE;
@@ -22,7 +24,31 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-    const [ SelectMonedas ] = useSelectMonedas('Elige tu moneda', monedas);
+    const [criptos, setCriptos] = useState([]);
+
+    const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu moneda', monedas);
+
+    useEffect( () => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            const arrayCriptos = resultado.Data.map( cripto => {
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    name: cripto.CoinInfo.FullName
+                }
+                return objeto;
+            });
+
+            setCriptos(arrayCriptos);
+
+        }
+
+        consultarAPI();
+    }, []);
 
     return (
         <form>
